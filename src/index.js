e9980cb4810e814ed98ca4f66d2fb85a85a5c98a
ssandorov <stefansandorovssb@gmail.com>
2022-05-25 20:18:38 +0200

    Preparamos los documentos para el proyecto

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-// Importamos la función del archivo componentes JS
-import {saludar} from './js/componentes.js';
 // Importamos el archivo de estilos global
 import './styles.css';
 
-saludar('Stefan');
+console.log('Hola Mundo');
